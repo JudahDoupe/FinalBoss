b0f0a2694143edfe95d1384fa0c2627d133b2dff
Judah Doupe <judahdoupe@gmail.com>
2018-12-21 22:08:06 -0500

    updated camera controls

--- Assets/Src/Fight/Fight.cs
+++ Assets/Src/Fight/Fight.cs
@@ -8,7 +8,7 @@ using UnityEngine.Networking;
 public class Fight : MonoBehaviour
 {
     public const int ActionsPerTurn = 5;
-    public const int MaxPlayers = 2;
+    public const int MaxPlayers = 1;
     
     //Fight
     public static bool GameInProgress = false;
--- Assets/Src/Framework/CameraController.cs
+++ Assets/Src/Framework/CameraController.cs
@@ -1,14 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public Vector3 Offset = new Vector3(0, 2, -2);
+    public float MoveSpeed = 5;
+    public float DragSpeed = 5;
+    public float ZoomSpeed = 5;
+
+    private Camera _camera;
+    private Vector3 _dragOrigin;
+    private Vector3 _cameraOrigin;
+    private Vector3 _targetPosition;
+    private Vector3 _targetRootPosition;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+        _targetPosition = transform.localPosition;
+        _targetRootPosition = transform.parent.position;
+    }
 
     void LateUpdate()
     {
-        if(Board.GetToken(Fight.ActivePlayer) != null)
-            transform.position = Vector3.Lerp(transform.position, Board.GetToken(Fight.ActivePlayer).transform.position + Offset, Time.deltaTime);
+        var move = Drag() || Rotate() || Zoom();
+        transform.parent.position = Vector3.Lerp(transform.parent.position, _targetRootPosition, Time.smoothDeltaTime * MoveSpeed);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, _targetPosition, Time.smoothDeltaTime * MoveSpeed);
+    }
+
+    private bool Drag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _dragOrigin = Input.mousePosition;
+            _cameraOrigin = _targetRootPosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            var speed = DragSpeed * _targetPosition.y;
+            Vector3 pos = _camera.ScreenToViewportPoint(Input.mousePosition - _dragOrigin);
+            Vector3 move = new Vector3(pos.x * speed, 0, pos.y * speed);
+            move = transform.parent.TransformDirection(move);
+            _targetRootPosition = _cameraOrigin - move;
+            return true;
+        }
+        return false;
+    }
+    private bool Rotate()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _dragOrigin = Input.mousePosition;
+            _cameraOrigin = _targetRootPosition;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 pos = _camera.ScreenToViewportPoint(Input.mousePosition - _dragOrigin);
+            transform.parent.Rotate(Vector3.up, pos.x * DragSpeed * 36);
+            _dragOrigin = Input.mousePosition;
+            return true;
+        }
+        return false;
+    }
+    private bool Zoom()
+    {
+        var scroll = Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed;
+        if (scroll == 0) return false;
+        var target = _targetPosition + transform.parent.InverseTransformDirection(transform.forward) * scroll;
+        if (target.y > 1 && target.y < 3)
+        {
+            _targetPosition = target;
+        }
+        return true;
     }
 }
